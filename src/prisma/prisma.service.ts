d2bc54b013488d47b548a1878a85abffb1d6e632
Titux Metal (MBP) <github@lgdweb.fr>
2023-01-28 16:29:14 +0100

    Adding prisma.service.ts file in prisma; adds the prisma service

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PrismaClient } from '@prisma/client'
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleDestroy, OnModuleInit
+{
+  constructor(config: ConfigService) {
+    const url = config.getOrThrow<string>('DATABASE_URL')
+    super({ datasources: { db: { url } } })
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect()
+  }
+
+  async onModuleInit() {
+    await this.$connect()
+  }
+}
