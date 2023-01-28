844aafeba1a0c86c2ce6672eb4fb3bbef0805c25
Titux Metal (MBP) <github@lgdweb.fr>
2023-01-28 16:31:09 +0100

    Updating app.module.ts file; makes the prisma module available in the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 
 import { AuthModule } from '~/auth'
+import { PrismaModule } from '~/prisma'
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, cache: true }),
+    AuthModule,
+    PrismaModule
+  ],
   controllers: [],
   providers: []
 })
