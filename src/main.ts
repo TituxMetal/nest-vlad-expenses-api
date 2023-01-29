87bc5b1e3d1ad5ca26e39d5f6de61ad957fd4f5e
Titux Metal (MBP) <github@lgdweb.fr>
2023-01-29 07:46:59 +0100

    Updating main.ts file; adds session middleware

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import * as session from 'express-session'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const config = app.get(ConfigService)
 
+  const sessionSecret = config.getOrThrow<string>('SESSION_SECRET')
+
+  app.use(
+    session({ secret: sessionSecret, resave: false, saveUninitialized: false })
+  )
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
 
-  const config = app.get(ConfigService)
   const port = config.getOrThrow<number>('PORT')
 
   await app.listen(port)
