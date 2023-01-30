253e7c8ee7ccf920fa99018b3d8663edca75053b
Titux Metal (MBP) <github@lgdweb.fr>
2023-01-30 06:01:53 +0100

    Updating app.module.ts file; adds the user module and provides the session guard globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,23 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { APP_INTERCEPTOR } from '@nestjs/core'
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core'
 
-import { AuthModule, TransformInterceptor } from '~/auth'
+import { AuthModule, SessionGuard, TransformInterceptor } from '~/auth'
 import { PrismaModule } from '~/prisma'
+
+import { UserModule } from './user/user.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, cache: true }),
     AuthModule,
-    PrismaModule
+    PrismaModule,
+    UserModule
   ],
   controllers: [],
-  providers: [{ provide: APP_INTERCEPTOR, useClass: TransformInterceptor }]
+  providers: [
+    { provide: APP_INTERCEPTOR, useClass: TransformInterceptor },
+    { provide: APP_GUARD, useClass: SessionGuard }
+  ]
 })
 export class AppModule {}
