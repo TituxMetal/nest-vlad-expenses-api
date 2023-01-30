b82e6f9ba5d868b41140c5f08eb2afcb1c0332ee
Titux Metal (MBP) <github@lgdweb.fr>
2023-01-30 09:05:17 +0100

    Adding expense.service.ts file in expense; implements CRUD features

---
+++ src/expense/expense.service.ts
@@ -0,0 +1,68 @@
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException
+} from '@nestjs/common'
+import { Expense } from '@prisma/client'
+
+import { PrismaService } from '~/prisma'
+
+import { CreateDto, UpdateDto } from './dto'
+
+@Injectable()
+export class ExpenseService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  getAllByUserId(userId: string): Promise<Expense[]> {
+    return this.prisma.expense.findMany({ where: { userId } })
+  }
+
+  async getUserExpenseById(
+    userId: string,
+    expenseId: string
+  ): Promise<Expense> {
+    const expense = await this.prisma.expense.findFirst({
+      where: { id: expenseId }
+    })
+
+    if (!expense) {
+      throw new NotFoundException('Resource not found.')
+    }
+
+    if (expense.userId !== userId) {
+      throw new ForbiddenException('Access to resource unauthorized.')
+    }
+
+    return expense
+  }
+
+  async create(userId: string, dto: CreateDto): Promise<Expense> {
+    const newExpense = await this.prisma.expense.create({
+      data: { ...dto, userId }
+    })
+
+    return newExpense
+  }
+
+  async updateById(
+    userId: string,
+    expenseId: string,
+    dto: UpdateDto
+  ): Promise<Expense> {
+    const { id } = await this.getUserExpenseById(userId, expenseId)
+    const updatedExpense = await this.prisma.expense.update({
+      where: { id },
+      data: { ...dto }
+    })
+
+    return updatedExpense
+  }
+
+  async deleteById(userId: string, expenseId: string): Promise<Expense> {
+    const expense = await this.getUserExpenseById(userId, expenseId)
+
+    await this.prisma.expense.delete({ where: { id: expense.id } })
+
+    return expense
+  }
+}
