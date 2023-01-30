634af5122a74b9ea6a608e533b74c7576a2c88fd
Titux Metal (MBP) <github@lgdweb.fr>
2023-01-30 09:09:49 +0100

    Updating app.module.ts file; adds expense module to the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,17 @@ import { ConfigModule } from '@nestjs/config'
 import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core'
 
 import { AuthModule, SessionGuard, TransformInterceptor } from '~/auth'
+import { ExpenseModule } from '~/expense'
 import { PrismaModule } from '~/prisma'
-
-import { UserModule } from './user/user.module'
+import { UserModule } from '~/user'
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, cache: true }),
     AuthModule,
     PrismaModule,
-    UserModule
+    UserModule,
+    ExpenseModule
   ],
   controllers: [],
   providers: [
