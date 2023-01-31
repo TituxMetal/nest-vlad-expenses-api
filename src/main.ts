54a5242bef3034e0992b7345b1739802292fc17b
Titux Metal (MBP) <github@lgdweb.fr>
2023-01-31 09:21:41 +0100

    Updating main.ts file; adds redis store and redis client for session middleware

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,35 @@
 import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import * as connectRedis from 'connect-redis'
 import * as session from 'express-session'
+import { createClient } from 'redis'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const config = app.get(ConfigService)
-
   const sessionSecret = config.getOrThrow<string>('SESSION_SECRET')
+  const RedisStore = connectRedis(session)
+  const redisClient = createClient({
+    url: config.getOrThrow<string>('REDIS_URL'),
+    legacyMode: true
+  })
 
   app.use(
-    session({ secret: sessionSecret, resave: false, saveUninitialized: false })
+    session({
+      secret: sessionSecret,
+      resave: false,
+      saveUninitialized: false,
+      store: new RedisStore({ client: redisClient })
+    })
   )
+
+  await redisClient.connect().catch(error => {
+    throw error
+  })
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
