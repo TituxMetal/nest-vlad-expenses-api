0c4b906cde7c44701fecfa5bc76b48792f3bacd5
Titux Metal (MBP) <github@lgdweb.fr>
2023-02-03 08:30:57 +0100

    Updating userSession.type.ts file in auth/types; adds role property to UserSessionData type

--- src/auth/types/userSession.type.ts
+++ src/auth/types/userSession.type.ts
@@ -3,6 +3,7 @@ import type { Session } from 'express-session'
 export type UserSessionData = {
   id: string
   email: string
+  role: string
 }
 
 export type UserSession = Session & Record<'user', UserSessionData>
