76e3a41dff3a0f724daf1f3c042adb9b4531c588
Titux Metal <github@lgdweb.fr>
2023-02-10 10:06:03 +0100

    Unit test/auth (#13)
    * Updating auth.service.ts file; fixes typo in login method, remove cause property in thrown exception
    
    * Updating auth.controller.spec.ts file; adds the tests for auth controller
    
    * Updating auth.service.spec.ts file; adds the tests for auth service

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,12 +1,31 @@
 import { Test } from '@nestjs/testing'
+import { Role } from '@prisma/client'
 
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
+import { AuthDto } from './dto'
+import { UserEntity } from './entity'
+import { UserSession } from './types'
+
+const _userEntity = new UserEntity({
+  id: 'abc123',
+  email: 'test@test.com',
+  role: Role.USER
+})
+const _authDto: AuthDto = {
+  email: 'test@test.com',
+  password: 'password'
+}
 
 describe('AuthController', () => {
-  const authServiceMock = jest.fn()
+  const authServiceMock = jest.fn().mockReturnValue({
+    signup: jest.fn().mockResolvedValue(_userEntity),
+    login: jest.fn().mockResolvedValue(_userEntity)
+  })
 
   let controller: AuthController
+  let result
+  let session
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
@@ -17,7 +36,47 @@ describe('AuthController', () => {
     controller = module.get<AuthController>(AuthController)
   })
 
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
+
   it('should be defined', () => {
     expect(controller).toBeDefined()
+  })
+
+  describe('signup()', () => {
+    beforeEach(async () => {
+      session = { user: undefined } as UserSession
+      result = await controller.signup(_authDto, session)
+    })
+
+    describe('when called', () => {
+      it('should return the created user', async () => {
+        expect(result).toEqual(_userEntity)
+      })
+
+      it('should add user info in the session', async () => {
+        expect(session.user).toBeDefined()
+        expect(session).toEqual({ user: _userEntity })
+      })
+    })
+  })
+
+  describe('login()', () => {
+    beforeEach(async () => {
+      session = { user: undefined } as UserSession
+      result = await controller.login(_authDto, session)
+    })
+
+    describe('when called', () => {
+      it('should return the logged in user', async () => {
+        expect(result).toEqual(_userEntity)
+      })
+
+      it('should add user info in the session', async () => {
+        expect(session.user).toBeDefined()
+        expect(session).toEqual({ user: _userEntity })
+      })
+    })
   })
 })
