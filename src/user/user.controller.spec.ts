630c16a9b697e2c3e8e6e3130cab480ae12afcd0
Titux Metal (MBP) <github@lgdweb.fr>
2023-02-12 05:48:24 +0100

    Updating user.controller.spec.ts file; adds the tests for user controller

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,12 +1,25 @@
 import { Test } from '@nestjs/testing'
+import { Role } from '@prisma/client'
+
+import { UserEntity } from '~/auth'
 
 import { UserController } from './user.controller'
 import { UserService } from './user.service'
 
 describe('UserController', () => {
-  const userServiceMock = jest.fn()
+  const userId = 'userid123'
+  const userEntity = new UserEntity({
+    id: userId,
+    email: 'test@test.com',
+    role: Role.USER
+  })
+  const userServiceMock = jest.fn().mockReturnValue({
+    getMe: jest.fn().mockResolvedValue(userEntity),
+    getAll: jest.fn().mockResolvedValue([userEntity])
+  })
 
   let controller: UserController
+  let result
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
@@ -18,10 +31,30 @@ describe('UserController', () => {
   })
 
   afterEach(() => {
-    jest.resetAllMocks()
+    jest.restoreAllMocks()
   })
 
   it('should be defined', () => {
     expect(controller).toBeDefined()
+  })
+
+  describe('getMe()', () => {
+    describe('when called', () => {
+      it('should return logged in user info', async () => {
+        result = await controller.getMe(userId)
+
+        expect(result).toEqual(userEntity)
+      })
+    })
+  })
+
+  describe('getAll()', () => {
+    describe('when called', () => {
+      it('should return all users', async () => {
+        result = await controller.getAll()
+
+        expect(result).toEqual([userEntity])
+      })
+    })
   })
 })
